12cc35f238bcc93f3e6af3cb065fbe176e13ca21
Ayesha Afzal <65703527+AyeshaAfzal91@users.noreply.github.com>
2025-04-19 00:26:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -34,5 +34,26 @@ function calculate() {
 }
 
 function showFormula() {
-    alert(`Performance / TCO = Performance / (Capital Cost + Operational Cost)`);
+    const formulaText = `
+        Performance / TCO = Performance / (C_capital + C_operational)
+        
+        C_capital = n_gpu * (
+                        C_gpu
+                      + C_node_server / GPUs_per_node
+                      + C_node_infrastructure / GPUs_per_node
+                      + C_node_facility / GPUs_per_node )
+                  + C_software
+
+        C_operational = C_operational_perYear * lifetime
+
+        where:
+
+        C_operational_perYear = n_gpu * (
+                      (C_electricityperkWh - C_heatreuseperkWh) * W_gpu * systemusage / 1000
+                    + C_coolingperkWh * (W_node_baseline * systemusage / GPUs_per_node + W_gpu * systemusage) / 1000
+                    + C_node_maintenance / GPUs_per_node )
+                  + C_depreciation
+                  + C_subscription
+                  + C_uefficiency`;
+    alert(formulaText);
 }
