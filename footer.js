67862231d7f5c6ebeb73e65efc2742f41bb57a59
Ayesha Afzal <65703527+AyeshaAfzal91@users.noreply.github.com>
2025-06-20 20:38:26 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -19,11 +19,11 @@ footer.style.borderTop = "1px solid #ddd";
 
 // Create BibTeX download link (if needed)
 const bibtexContent = `
-@misc{Afzal2025PerformancePerTCO,
+@misc{Afzal2025Wattlytics,
   author       = {Ayesha Afzal},
-  title        = {Performance Per TCO Calculator},
+  title        = {Wattlytics: Analytics for Smarter Energy Decisions},
   year         = {2025},
-  howpublished = {\\url{https://perfpertco.netlify.app}},
+  howpublished = {\\url{https://wattlytics.netlify.app}},
   note         = {Accessed: ${todaysDate}. Licensed under CC BY 4.0.}
 }
 `;
@@ -45,8 +45,8 @@ footer.innerHTML = `
   This website and its content are licensed under 
   <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener">CC BY 4.0</a> and require citation when used.<br><br>
   <strong>Please cite this tool as</strong><br>
-  <em>Ayesha Afzal (2025). Performance Per TCO Calculator. 
-  <a href="https://perfpertco.netlify.app" target="_blank">https://perfpertco.netlify.app</a> 
+  <em>Ayesha Afzal (2025). Wattlytics: Analytics for Smarter Energy Decisions. 
+  <a href="https://perfpertco.netlify.app" target="_blank">https://wattlytics.netlify.app</a> 
   Accessed: ${todaysDate}. Licensed under CC BY 4.0.</em><br>
 `;
 
