e306955fdad3379a95603ecfa44dd316119fee13
Ayesha Afzal <65703527+AyeshaAfzal91@users.noreply.github.com>
2026-01-29 04:54:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1545,7 +1545,7 @@ const tcoLayout = {
     tickfont: { size: 18 },
     automargin: true
   },
-  height: 600,
+  height: 300,
   margin: { t: 60, b: 80, l: 80, r: 200 }, // Extra right margin for vertical legend
 legend: {
   orientation: 'v',   // Horizontal for bottom placement
